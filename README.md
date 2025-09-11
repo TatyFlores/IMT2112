cf8ba6e50d09c4f61f3befb4caf5fb5640a9d422
TatyFlores <tlflores@uc.cl>
2025-09-11 20:44:48 -0300

    Avance T2

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 Repositorio sobre la clase IMT2112
 
 Este repositorio fue creado para almacenar el código procesado para las tareas del curso IMT2112.
+
+Algunos comandos útiles:
+* cd nombre_carpeta: es para moverse por los directorios
+* cd ..: es para devolverse una carpeta 
+* mkdir nombre_carpeta: es para crear una nueva carpeta
+* ls: es para ver los archivos que se encuentran en el directorio actual
+
+Para compilar archivos de C++:
+
+
+Para compilar archivos con MPI:
+* Compilar: mpic++ tarea2.cpp -std=c++11
+* Correr (con 2 procesos): mpirun -np 2 ./a.out
--- Tarea 2/tarea2.cpp
+++ Tarea 2/tarea2.cpp
@@ -4,15 +4,31 @@
 #include <mpi.h>
 
 
-int main(int argc, char** argv) {
-
+int main(int argc, char** argv)
+{
     MPI_Init(&argc, &argv);
 
-    MPI_Finalize();
-
     int size, rank;
-MPI_Comm_size(MPI_COMM_WORLD, &size);
-MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &size);  // número de procesos (p)
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);  // id del proceso (r)
+
+    // Queremos leer las dimensiones del archivo
+
+    int nrows = 0, ncols = 0;
+    if (rank == 0)
+    {
+        std::ifstream file("matriz.txt");
+        if (!file.is_open()) {
+            std::cerr << "Unable to open file.'\n";
+            MPI_Abort(MPI_COMM_WORLD, 1);
+        }
+        file >> nrows >> ncols;  // 
+        file.close();
+    }
+
+    std::cout << "Filas: " << nrows << ", Columnas: " << ncols << std::endl;
+
+    MPI_Finalize();
 
     return 0;
 }
