c49e18d164167525c21d6a95b823d5b01d2b6ae7
TatyFlores <tlflores@uc.cl>
2025-09-12 17:43:56 -0300

    T2: Corrección de errores pt.2

--- Tarea 2/submit_cpp.sh
+++ Tarea 2/submit_cpp.sh
@@ -13,11 +13,20 @@
 echo "start script"
 date
 
+if [ -f /etc/profile ]; then
+  source /etc/profile
+fi
+if [ -f /etc/profile.d/modules.sh ]; then
+  source /etc/profile.d/modules.sh
+elif [ -f /usr/share/Modules/init/bash ]; then
+  source /usr/share/Modules/init/bash
+fi
+
 module purge
 module load gcc
 module load openmpi
 
-mpic++ tarea2.cpp
+mpic++ tarea2.cpp -std=c++11
 time mpirun -np 2 a.out
 
 echo "end script"
