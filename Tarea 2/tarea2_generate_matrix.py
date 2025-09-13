e70f314cd36879e55b920d722350afc09112d36f
TatyFlores <tlflores@uc.cl>
2025-09-13 18:00:48 -0300

    T2: Corrección de errores pt.42

--- Tarea 2/tarea2_generate_matrix.py
+++ Tarea 2/tarea2_generate_matrix.py
@@ -22,6 +22,6 @@ def write_matrix(matrix, filename='matrix.txt'):
                 f.write(str(mat[i,j])+"\n")
         f.write("end")
 
-ndim = 1300
+ndim = 1400
 mat = generate_matrix(ndim)
 write_matrix(mat)
